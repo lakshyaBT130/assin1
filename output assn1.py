3a7bb277304588fa6d1b1bb970a3607a325d56e5
lakshyaBT130 <98959054+lakshyaBT130@users.noreply.github.com>
2022-04-26 00:03:25 +0530

    Add files via upload

---
+++ output assn1.py
@@ -0,0 +1,31 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:/Users/laksh/Downloads/assin1.py =================
+write three numbers = 
+5554
+545
+154
+avg. 2084.3333333333335
+Please enter you Gross Income to the nearest penny and your income should be more than $10000
+48484515
+Please enter the number of dependents you have
+5
+Your taxable income is = $ 48459515.0
+Your income tax= $ 9691903.0
+your SID515
+your namehvf
+your genderjbv
+your coursegbebg
+your CGPA9
+[515, 'hvf', 'jbv', 'gbebg', 9.0]
+Enter the marks of 5 students
+8848
+554
+544554
+48
+45
+marks in sorted manner [45, 48, 554, 8848, 544554]
+specified list after removing 4th element  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']
+>>> 
